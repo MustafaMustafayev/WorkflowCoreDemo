2146ac3d611b69f592229318257c1ac91893639c
Mustafa112263 <mmutafayev0001@gmail.com>
2020-04-10 14:35:31 +0400

    work flow core demo

---
+++ WorkflowCoreWebsite/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+using WorkflowCore.Interface;
+using WorkflowCoreWebsite.Models;
+using WorkflowCoreWebsite.Workflows;
+
+namespace WorkflowCoreWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWorkflowHost workflowHost;
+
+        public HomeController(IWorkflowHost workflowHost)
+        {
+            this.workflowHost = workflowHost;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Contact(ContactForm model)
+        {
+            if (ModelState.IsValid)
+            {
+                workflowHost.StartWorkflow(nameof(ContactFormWorkflow), model);
+
+                TempData["Message"] = "Thanks for reaching out!";
+                return RedirectToAction(nameof(Contact));
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Ack(Guid id)
+        {
+            // You could of course do a lot more here like ask for more information
+            // and publish that data as part of the event.
+
+            workflowHost.PublishEvent("ContactFormAcknowledged", id.ToString(), null);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WorkflowCoreWebsite/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WorkflowCore.Interface;
+using WorkflowCoreWebsite.Models;
+using WorkflowCoreWebsite.Workflows;
+
+namespace WorkflowCoreWebsite
+{
+    public class Startup
+    {
+        private IWorkflowHost host;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => false;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddWorkflow();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // Start the workflow host
+            host = app.ApplicationServices.GetService<IWorkflowHost>();
+            host.RegisterWorkflow<ContactFormWorkflow, ContactForm>();
+            host.Start();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
